c7acb2269f95455c45337fe7e74c5df8f5c12af7
Justina Kuchta <justyna.kuchta@gmail.com>
2019-08-19 21:13:30 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -13,3 +13,26 @@ input.addEventListener('keydown', function(e) {
     console.log(e.key)
   }
 });
+
+let divs = document.querySelectorAll('div');
+ 
+function bubble(e) {
+  console.log(this.firstChild.nodeValue.trim() + ' bubbled');
+}
+ 
+for (let i = 0; i < divs.length; i++) {
+  divs[i].addEventListener('click', bubble);
+}
+
+const divs = document.querySelectorAll('div');
+ 
+function bubble(e) {
+  // stop! that! propagation!
+  e.stopPropagation();
+ 
+  console.log(this.firstChild.nodeValue.trim() + ' bubbled');
+}
+ 
+for (let i = 0; i < divs.length; i++) {
+  divs[i].addEventListener('click', bubble);
+}
